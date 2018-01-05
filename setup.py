ac6489d4cd8dc250c2b9aae6fc7eccf6f9eb2dde
jtloong <joshua.t.loong@gmail.com>
2018-01-05 15:43:28 -0500

    Initial logic working, erorr when installed as a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chow_test',
+      version='0.1',
+      description='Package to calculate Chow break statistics.',
+      url='http://github.com/jtloong/chow_test',
+      author='Joshua Loong',
+      author_email='joshua.t.loong@gmail.com',
+      license='MIT',
+      packages=['chow_test'],
+      zip_safe=False)
