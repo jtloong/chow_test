bcb095c5cd4b7a50c0cab41d5c5fe12c8be36795
jtloong <joshua.t.loong@gmail.com>
2018-01-10 16:35:47 -0500

    Added docstrings and adjusted spacing to follow PEP8

--- chow_test/__init__.py
+++ chow_test/__init__.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 def f_value(breakpoint, index, y, x):
+     """Return the f-value of the Chow break test."""
     def find_rss (y, x):
+         """Return the residual sum of squares and data length."""
         A = np.vstack([x, np.ones(len(x))]).T
         rss = np.linalg.lstsq(A, y)[1]
         length = len(y)
         return [rss, length]
-    
+
     rss_total, n_total = find_rss(y, x)
     rss_1, n_1 = find_rss(np.take(y, np.where(index < breakpoint))[0], np.take(x, np.where(index < breakpoint))[0])
     rss_2, n_2 = find_rss(np.take(y, np.where(index > (breakpoint - 1)))[0], np.take(x, np.where(index > (breakpoint - 1)))[0])
 
     chow_nom = (rss_total - (rss_1 + rss_2)) / 2
-    chow_denom = (rss_1+rss_2) / (n_1 + n_2 - (2 * 2))
+    chow_denom = (rss_1 + rss_2) / (n_1 + n_2 - (2 * 2))
     chow = chow_nom / chow_denom
     return chow
