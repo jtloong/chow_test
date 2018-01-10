588f7adc66c881e34906229a56f69f298e019e9b
jtloong <joshua.t.loong@gmail.com>
2018-01-10 15:46:46 -0500

    Made changes, removed sklearn/pandas dependancies, made input simpler, cleaner code

--- chow_test/__init__.py
+++ chow_test/__init__.py
@@ -1,22 +1,17 @@
-import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
 
-def f_value(break_point, data, timeColumn, x_column, y_column):
-    def find_rss (data, x_column, y_column):
-        x = data[[x_column]]
-        y = data[y_column]
-        lm = LinearRegression()
-        lm.fit(x, y)
-        rss = np.sum(np.square(y - lm.predict(x)))
-        return rss
-    data = data.dropna()
-    rss_total = find_rss(data, x_column, y_column)
-    rss_1 = find_rss(data[data[timeColumn] < break_point], x_column, y_column)
-    rss_2 = find_rss(data[data[timeColumn] > (break_point - 1)], x_column, y_column)
-    n_1 = len(data[data[timeColumn] < break_point])
-    n_2 = len(data[data[timeColumn] > (break_point - 1)])
+def f_value(breakpoint, index, y, x):
+    def find_rss (y, x):
+        A = np.vstack([x, np.ones(len(x))]).T
+        rss = np.linalg.lstsq(A, y)[1]
+        length = len(y)
+        return [rss, length]
+
+    rss_total, n_total = find_rss(y, x)
+    rss_1, n_1 = find_rss(np.take(y, np.where(index < breakpoint))[0], np.take(x, np.where(index < breakpoint))[0])
+    rss_2, n_2 = find_rss(np.take(y, np.where(index > (breakpoint - 1)))[0], np.take(x, np.where(index > (breakpoint - 1)))[0])
+
     chow_nom = (rss_total - (rss_1 + rss_2)) / 2
-    chow_denom = (rss_1+rss_2) / (n_1 + n_2 - (2 *2))
+    chow_denom = (rss_1+rss_2) / (n_1 + n_2 - (2 * 2))
     chow = chow_nom / chow_denom
     return chow
