4ba2af16f62c5ee5b4cea31d21762fb508a5642f
RUrlus <RUrlus@users.noreply.github.com>
2018-01-11 00:23:44 +0100

    Update __init__.py
    A few suggestions on how you could clean up the code a bit. As I am not sure what kind of inputs you expect I couldn't test properly.

--- chow_test/__init__.py
+++ chow_test/__init__.py
@@ -1,19 +1,61 @@
 import numpy as np
 
-def f_value(breakpoint, index, y, x):
-     """Return the f-value of the Chow break test."""
-    def find_rss (y, x):
-         """Return the residual sum of squares and data length."""
-        A = np.vstack([x, np.ones(len(x))]).T
+# now you can use this function at other points
+def find_rss (y, x):
+        """
+        Return the residual sum of squares and data length.
+        # Docstring doesns't mention the types of the input or the expected dimensions
+        # example of numpy style docstring:
+    
+        Parameters
+        ----------
+        x : ndarray | (N, 1)
+            The ...
+        
+        Output
+        ------
+        rss : ...
+              Resid...
+        length : ...
+            
+        """
+        # The code is not robust for x inputs of 1-D or higher
+        # By taking the 'len' you assume 0-D array
+        # equivalent to 'if x.ndim == 1:'
+        if x.ndim == 1:
+            # add dimension to x, now col vector
+            x = x[:, None]
+        A = np.hstack((x, np.ones(x.shape)))
         rss = np.linalg.lstsq(A, y)[1]
         length = len(y)
-        return [rss, length]
+        # tuples are faster to create than lists
+        return (rss, length)
 
+
+def f_value(breakpoint, index, y, x):
+    """Return the f-value of the Chow break test.
+    # Docstring doesns't mention the types of the input or the expected dimensions
+    example of numpy style docstring:
+    
+    Parameters
+    ----------
+    x : ndarray | (N, 1)
+        The ...
+    
+    Output
+    ------
+    *f-value* : float
+                f-value of the Chow break test
+    """
+    
     rss_total, n_total = find_rss(y, x)
-    rss_1, n_1 = find_rss(np.take(y, np.where(index < breakpoint))[0], np.take(x, np.where(index < breakpoint))[0])
-    rss_2, n_2 = find_rss(np.take(y, np.where(index > (breakpoint - 1)))[0], np.take(x, np.where(index > (breakpoint - 1)))[0])
+    # Fancy indexing is faster and gives cleaner code
+    rss_1, n_1 = find_rss(y[index < breakpoint], x[index < breakpoint])
+    rss_2, n_2 = find_rss(y[index > (breakpoint - 1)], x[index > (breakpoint - 1)])
 
     chow_nom = (rss_total - (rss_1 + rss_2)) / 2
-    chow_denom = (rss_1 + rss_2) / (n_1 + n_2 - (2 * 2))
-    chow = chow_nom / chow_denom
-    return chow
+    # 2 * 2 = 4
+    # chow_denom = (rss_1 + rss_2) / (n_1 + n_2 - (2 * 2))
+    chow_denom = (rss_1 + rss_2) / (n_1 + n_2 - 4)
+    # No need to define and than return
+    return chow_nom / chow_denom
