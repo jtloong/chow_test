cbbd4de41583c327acc99f529e13bcd8dc85ba35
jtloong <joshua.t.loong@gmail.com>
2018-01-31 21:25:20 -0500

    Remade the function to be simpler and have tests

--- chow_test/__init__.py
+++ chow_test/__init__.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-def f_value(breakpoint, index, y, x):
-     """Return the f-value of the Chow break test."""
+def f_value(y1, x1, y2, x2):
+    """Return the f-value of the Chow break test."""
     def find_rss (y, x):
-         """Return the residual sum of squares and data length."""
+        """Return the residual sum of squares and data length."""
         A = np.vstack([x, np.ones(len(x))]).T
         rss = np.linalg.lstsq(A, y)[1]
         length = len(y)
         return [rss, length]
 
-    rss_total, n_total = find_rss(y, x)
-    rss_1, n_1 = find_rss(np.take(y, np.where(index < breakpoint))[0], np.take(x, np.where(index < breakpoint))[0])
-    rss_2, n_2 = find_rss(np.take(y, np.where(index > (breakpoint - 1)))[0], np.take(x, np.where(index > (breakpoint - 1)))[0])
+    rss_total, n_total = find_rss(np.append(y1, y2), np.append(x1, x2))
+    rss_1, n_1 = find_rss(y1, x1)
+    rss_2, n_2 = find_rss(y2, x2)
 
     chow_nom = (rss_total - (rss_1 + rss_2)) / 2
     chow_denom = (rss_1 + rss_2) / (n_1 + n_2 - (2 * 2))
