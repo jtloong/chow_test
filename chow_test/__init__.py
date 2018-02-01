79a24cd097ff10b90847cff340d446e198d4cd4a
jtloong <joshua.t.loong@gmail.com>
2018-01-31 21:34:45 -0500

    Added docstrngs to functions

--- chow_test/__init__.py
+++ chow_test/__init__.py
@@ -1,13 +1,29 @@
 import numpy as np
 
 def f_value(y1, x1, y2, x2):
-    """Return the f-value of the Chow break test."""
+    """This is the f_value function for the Chow Break test package
+    Args:
+        y1: Array like y-values for data preceeding the breakpoint
+        x1: Array like x-values for data preceeding the breakpoint
+        y2: Array like y-values for data occuring after the breakpoint
+        x2: Array like x-values for data occuring after the breakpoint
+
+    Returns:
+        F-value: Float value of chow break test
+    """
     def find_rss (y, x):
-        """Return the residual sum of squares and data length."""
+        """This is the subfunction to find the residual sum of squares for a given set of data
+        Args:
+            y: Array like y-values for data subset
+            x: Array like x-values for data subset
+
+        Returns:
+            rss: Returns residual sum of squares of the linear equation represented by that data
+            length: The number of n terms that the data represents
+        """
         A = np.vstack([x, np.ones(len(x))]).T
         rss = np.linalg.lstsq(A, y)[1]
         length = len(y)
-        # tuples are faster to create than lists
         return (rss, length)
 
 
@@ -16,8 +32,5 @@ def f_value(y1, x1, y2, x2):
     rss_2, n_2 = find_rss(y2, x2)
 
     chow_nom = (rss_total - (rss_1 + rss_2)) / 2
-    # 2 * 2 = 4
-    # chow_denom = (rss_1 + rss_2) / (n_1 + n_2 - (2 * 2))
     chow_denom = (rss_1 + rss_2) / (n_1 + n_2 - 4)
-    # No need to define and than return
     return chow_nom / chow_denom
